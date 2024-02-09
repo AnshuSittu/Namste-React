753485ac4a33de7257c3f20ba58f071c3b57916a
Anshu <anshu.possible@gmail.com>
2024-02-09 15:27:57 +0530

    nested sturcutre parent child

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
-const heading = React.createElement(
+
+const parent = React.createElement("div",
+{id: "parent"},
+React.createElement(
+    "div",
+    {id: "child"},
+    React.createElement("h1", {}, "I am h1 tag")
+    )
+);
+
+console.log(parent);
+
+
+
+
+
+
+
+/* const heading = React.createElement(
   "h1",
   { id: "heading" },
   "Hello From React !!"
 );
+ */
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+// root.render(heading);
+
+root.render(parent);
